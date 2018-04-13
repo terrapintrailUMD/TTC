e60d56dd61cd898b8e94f061261acdc75146ef5b
William Guo <wguo1@terpmail.umd.edu>
2018-04-12 20:28:01 -0400

    Temporary Fix for Intro Trip

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
-<!--This is the landing page which displays a slideshow of pictures-->
+<!--This is the about page which gives some general information about what TTC is and what
+time/place we meet.-->
 
 <!DOCTYPE html>
 <html>
 <head>
 <link rel="shortcut icon" type="image/x-icon" href="images/favicon.png">
-<title>Home</title>
+<title>HOME</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="w3.css">
@@ -16,7 +17,6 @@ body, html {
     font-family: "Inconsolata", sans-serif;
 	background-color: #000000;
 }
-.slides {display: none}
 </style>
 <body>
 
@@ -24,7 +24,7 @@ body, html {
 <div class="w3-top">
   <div class="w3-row w3-padding w3-black">
     <div class="w3-col s2">
-      <a href="#" class="w3-button w3-block w3-black">HOME</a>
+      <a href="index.php" class="w3-button w3-block w3-black">HOME</a>
     </div>
     <div class="w3-col s2">
       <a href="about.html" class="w3-button w3-block w3-black">ABOUT US</a>
@@ -45,84 +45,28 @@ body, html {
 </div>
 
 <!--Sets the format for the rest of the page-->
-<div class="w3-display-container">
+<div class="w3-sand w3-grayscale w3-large">
 
-<!--This displays all of the pictures-->
-<div class="slides w3-animate-right">
-  <img src="images/first.jpg" style="width:100%;">
-</div>
-<div class="slides w3-animate-right">
-  <img src="images/second.jpg" style="width:100%;">
-</div>
-<div class="slides w3-animate-right">
-  <img src="images/third.jpg" style="width:100%;">
-</div>
-<div class="slides w3-animate-right">
-  <img src="images/fourth.jpg" style="width:100%;">
-</div>
-<div class="slides w3-animate-right">
-  <img src="images/fifth.jpg" style="width:100%;">
-</div>
-<div class="slides w3-animate-right">
-  <img src="images/seventh.jpg" style="width:100%;">
-</div>
-<div class="slides w3-animate-right">
-  <img src="images/eighth.jpg" style="width:100%;">
-</div>
-<div class="slides w3-animate-right">
-  <img src="images/ninth.jpg" style="width:100%;">
-</div>
-<div class="slides w3-animate-right">
-  <img src="images/tenth.jpg" style="width:100%;">
-</div>
-
-<div class="w3-display-middle w3-center">
-    <span class="w3-text-white" style="font-size:5vw">Terrapin<br>Trail Club</span>
+<div class="w3-container" id="about">
+  <div class="w3-content" style="max-width:700px">
+    <h5 class="w3-center w3-padding-64_8"><span class="w3-tag w3-wide">INTRO TRIP</span></h5>
+    <p>Sign Up for Intro Trip at the following URL: <a href = "https://docs.google.com/forms/d/e/1FAIpQLSfsVzdAnBJ0EMupBcEngi7Do2llZSUr15OBbdd3c8ncSmrXug/viewform?c=0&w=1">Sign Up Link</a></p>
+	<p>Sign Ups close Friday April 13th at 9PM</p>
+	<p>You must be at the Monday Night Meeting on April 16th and fill out the form to be in consideration.</p>
+	<p>Additional info can be found at the URL above and questions can be answered by emailing officers@terrapintrailclub.org</p>
   </div>
-  
-<!--This will run the script to initiate button functionality-->
-<button class="w3-button w3-black w3-display-left" onclick="prevSlide()">&#10094;</button>
-  <button class="w3-button w3-black w3-display-right" onclick="nextSlide()">&#10095;</button>
 </div>
 
+<div class="w3-container" id="about">
+  <div class="w3-content" style="max-width:700px">
+    <h5 class="w3-center w3-padding-645"><span class="w3-tag w3-wide">WHAT IS INTRO TRIP?</span></h5>
+    <p><strong>Time:</strong>Friday April 20th to Sunday April 22nd</p>
+    <p><strong>Place:</strong> Seneca Rocks, WV</p>
+	<p><strong>How Many:</strong> Around 50 people</p>
+	<p><strong>Purpose:</strong>It's our largest trip of the semester where we lead beginners trip in hiking, kayaking, caving, rock climbing and mountain biking.</p>
+  </div>
+</div>
 
-<!--This is the script used to run the slideshow-->
-<script>
-var i = 1;
-var timer = null;
-slides(i);
-
-<!--This will go to the next slide-->
-function nextSlide()
-{
-	clearTimeout(timer)<!--This will clear the current timer-->
-	slides(++i)
-}
-
-<!--This will go to the previous slide-->
-function prevSlide()
-{
-	clearTimeout(timer)<!--This will clear the current timer-->
-	slides(--i)
-}
-
-<!--This will translate over n amount of slides-->
-function slides(n) {
-  var slideShow = document.getElementsByClassName("slides");
-  
-  var x = 0;
-  while (x < slideShow.length)<!--Set all slides to none-->
-  	slideShow[x++].style.display = "none";
-  
-  <!--If n is greater than the length of the slides set n to 1 or if n is less-->
-  <!--than 1, then set n to the length - 1. Or if it is not either one return n-->
-  i= n > slideShow.length ? 1 : (n < 1) ? slideShow.length:i
-  slideShow[i-1].style.display = "block";
-  
-  <!--Set a timer for 8.5 seconds to automatically transition to the next one-->
-  timer = setTimeout(nextSlide,8500)
-}
-</script>
 
 </div>
 
@@ -130,6 +74,7 @@ function slides(n) {
 <footer class="w3-center w3-light-grey w3-padding-48 w3-large">
   <p>Developed by William Guo</p>
 </footer>
+
 
 </body>
 </html>
